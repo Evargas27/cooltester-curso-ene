b2055a6cdbf4fecfde1588347880f315848f24f7
ASUS <ASUS@DESKTOP-8EII8FL.mshome.net>
2022-02-03 11:56:33 -0500

    Update Ejercicio1

--- SeleniumCursoEnero/src/test/java/com/ejercicios/java/Ejercicio1.java
+++ SeleniumCursoEnero/src/test/java/com/ejercicios/java/Ejercicio1.java
@@ -3,8 +3,8 @@ package com.ejercicios.java;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// Crear una clase (public static void main) con tres variables de tipo
-		// double e imprimir la suma, resta, multiplicación y división de las 3 variables. 
+		// Crear una clase (public static void main) con tres variables de tipo double 
+		//e imprimir la suma, resta, multiplicación y división de las 3 variables. 
 
 		double x = 1.5;
 		double y = 2.4;
@@ -17,12 +17,10 @@ public class Ejercicio1 {
 		double div = x / y / z;
 		double divRedondeo = Math.round(div*100.0)/100.0;
 						
-		System.out.println("Esta es la suma: " + sum);
-		//System.out.println(x "+" "+ y + z = " + sum);
-		System.out.println("Esta es la resta: " + resta);
-		System.out.println("Esta es la multiplicación: " + multRedondeo);
-		System.out.println("Esta es la división: " + divRedondeo);	
+		System.out.println("Suma: " + x + " + " + y + " + " + z + " = " + sum);
+		System.out.println("Resta: " + x + " - " + y + " - " + z + " = " + resta);
+		System.out.println("Multiplicación: " + x + " * " + y + " * " + z + " = " + multRedondeo);
+		System.out.println("División: " + x + " / " + y + " / " + z + " = " + divRedondeo);
 		
-		System.out.println("Hola mundo cruel");
 	}
 }
