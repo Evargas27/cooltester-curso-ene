091e4e49c9604dad6983d0df248aa6f4a4f2329f
ASUS <ASUS@DESKTOP-8EII8FL.mshome.net>
2022-02-07 22:12:00 -0500

    Add BarkingDog and MainClass

---
+++ SeleniumCursoEnero/src/test/java/com/ejercicios/java/MainClass.java
@@ -0,0 +1,9 @@
+package com.ejercicios.java;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println(BarkingDog.shouldWakeUp(false, 7)? "A LEVANTARSE!!!":"PUEDE SEGUIR DURMIENDO...");
+	}
+}
